21a8cbe8845f5b1e21c6a5d8153562cd233fd5e6
yujin <yujin@DESKTOP-UK9JJQG>
2022-03-21 14:42:05 +0900

    수업시간 예제

--- JavaPlz/src/ch01/ObjectTest2.java
+++ JavaPlz/src/ch01/ObjectTest2.java
@@ -20,6 +20,9 @@ public class ObjectTest2 {
 		System.out.println("c.name = "+ c.name);
 		System.out.println("c2.name = "+ c2.name);
 		System.out.println("c2.breeds = "+ c2.breeds);
+		
+		c.healing();
+		c2.healing();
 
 	}
 
