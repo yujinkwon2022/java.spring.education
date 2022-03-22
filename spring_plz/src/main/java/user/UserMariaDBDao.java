357e3ec869b4590a2a440731c38fec523da7baaf
yujin <yujin@DESKTOP-UK9JJQG>
2022-03-22 15:21:12 +0900

    1234

--- spring_plz/src/main/java/user/UserMariaDBDao.java
+++ spring_plz/src/main/java/user/UserMariaDBDao.java
@@ -1,6 +1,6 @@
 package user;
 
-public class UserMariaDBDao {
+public class UserMariaDBDao implements UserDao{
 	
 	public void getUser(String id) {
 		System.out.println("마리아 디비에서 아이디 "+ id +" 정보 조회");
--- spring_plz/src/main/java/user/UserOracleDao.java
+++ spring_plz/src/main/java/user/UserOracleDao.java
@@ -1,6 +1,6 @@
 package user;
 
-public class UserOracleDao {
+public class UserOracleDao implements UserDao{
 	
 	public void getUser(String id) {
 		System.out.println("오라클에서 아이디 " + id +"인 사원 정보 조회");
